42947ce37571a6ce4366f26e3599dfd559eb5ff8
Hekouo <237831196+hekouo@users.noreply.github.com>
2025-10-21 10:30:06 -0600

    chore(tw): add proper content globs to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,12 @@
+// tailwind.config.ts
 import type { Config } from "tailwindcss";
+
 export default {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./lib/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: { extend: {} },
   plugins: [],
